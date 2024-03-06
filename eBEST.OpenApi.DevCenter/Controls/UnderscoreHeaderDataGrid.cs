f99b55c1c746d98c693144259bae3c96cea0ec24
teranum <tera63614@gmail.com>
2024-03-06 21:19:40 +0900

    add UnderscoreHeaderDataGrid

---
+++ eBEST.OpenApi.DevCenter/Controls/UnderscoreHeaderDataGrid.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace eBEST.OpenApi.DevCenter.Controls
+{
+    public class UnderscoreHeaderDataGrid : DataGrid
+    {
+        public UnderscoreHeaderDataGrid()
+        {
+            AutoGeneratingColumn += (sender, e) =>
+            {
+                if (e.PropertyName.Contains("_"))
+                {
+                    e.Column.Header = e.PropertyName.Replace("_", "__");
+                }
+            };
+        }
+    }
+}
